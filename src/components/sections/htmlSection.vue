3e39ebefb69fab87a213ce697b9a07082f01796c
Simon Erlandsen <simon.erlandsen@gmail.com>
2019-03-27 14:00:26 +0100

    formatting template output in html tab

--- src/components/sections/htmlSection.vue
+++ src/components/sections/htmlSection.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h5 class="is-size-5">HTML</h5>
-    <pre v-highlightjs="patternVariantData.template"><code class="language-html"></code></pre>
-    <button class="button is-info margin-top" @click="copy(patternVariantData.template)">
+    <pre v-highlightjs="template"><code class="language-html"></code></pre>
+    <button class="button is-info margin-top" @click="copy(template)">
       <span class="icon">
         <i class="fas fa-copy"></i>
       </span>
@@ -14,10 +14,26 @@
 <script>
   import copyToClipboardMixin from '../../mixins/copyToClipboard'
   import shared from './shared'
+  import * as beautify from 'js-beautify'
 
   export default {
     name: 'html-section',
 
-    mixins: [copyToClipboardMixin, shared]
+    mixins: [copyToClipboardMixin, shared],
+
+    data() {
+      return {
+        regexToFindNewLines: /^(?:[\t ]*(?:\r?\n|\r))+/gm
+      }
+    },
+
+    computed: {
+      template() {
+        const templateWithoutNewLines = this.patternVariantData.template.replace(this.regexToFindNewLines, '')
+        const templateWithCorrectIndenting = beautify.html(templateWithoutNewLines)
+
+        return templateWithCorrectIndenting
+      }
+    }
   }
 </script>
