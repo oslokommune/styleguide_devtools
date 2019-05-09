388c35e4d7684965239ab881f63675d5954d3385
Simon Erlandsen <simon.erlandsen@gmail.com>
2019-05-09 09:10:11 +0200

    reverted some autoformatting.

--- src/components/atomicNav.vue
+++ src/components/atomicNav.vue
@@ -20,64 +20,76 @@
         </div>
       </div>
     </div>
-    <p class="menu-label" v-if="!tags.length">Global</p>
+    <p class="menu-label" v-if="!tags.length">
+      Global
+    </p>
     <ul class="menu-list" v-if="!tags.length">
       <atomic-nav-item
         :item="child"
         parentName="globals"
         v-for="(child, index) in atomicStructure.globals.children"
-        v-bind:key="index"
-      ></atomic-nav-item>
+        v-bind:key="index">
+      </atomic-nav-item>
     </ul>
-    <p class="menu-label" v-if="!tags.length">Atoms</p>
+    <p class="menu-label" v-if="!tags.length">
+      Atoms
+    </p>
     <ul class="menu-list" v-if="!tags.length">
       <atomic-nav-item
         :item="child"
         parentName="atoms"
         v-for="(child, index) in atomicStructure.atoms.children"
-        v-bind:key="index"
-      ></atomic-nav-item>
+        v-bind:key="index">
+      </atomic-nav-item>
     </ul>
-    <p class="menu-label" v-if="!tags.length">Molecules</p>
+    <p class="menu-label" v-if="!tags.length">
+      Molecules
+    </p>
     <ul class="menu-list" v-if="!tags.length">
       <atomic-nav-item
         :item="child"
         parentName="molecules"
         v-for="(child, index) in atomicStructure.molecules.children"
-        v-bind:key="index"
-      ></atomic-nav-item>
+        v-bind:key="index">
+      </atomic-nav-item>
     </ul>
-    <p class="menu-label" v-if="!tags.length">Organisms</p>
+    <p class="menu-label" v-if="!tags.length">
+      Organisms
+    </p>
     <ul class="menu-list" v-if="!tags.length">
       <atomic-nav-item
         :item="child"
         parentName="organisms"
         v-for="(child, index) in atomicStructure.organisms.children"
-        v-bind:key="index"
-      ></atomic-nav-item>
+        v-bind:key="index">
+      </atomic-nav-item>
     </ul>
     <p class="menu-label" v-if="!tags.length && pagesVisible">Pages</p>
     <ul class="menu-list" v-if="!tags.length && pagesVisible">
       <li>
-        <router-link :to="{ path: '/pageBuilder' }">Build</router-link>
+        <router-link :to="{ path: '/pageBuilder' }">
+          Build
+        </router-link>
       </li>
     </ul>
-    <p class="menu-label" v-if="tags.length">Tags</p>
+    <p class="menu-label" v-if="tags.length">
+      Tags
+    </p>
     <ul class="menu-list" v-if="tags.length">
       <atomic-tag-item
         :item="item"
         :tags="tags"
         v-for="(item, index) in patterns"
-        v-bind:key="index"
-      ></atomic-tag-item>
+        v-bind:key="index">
+      </atomic-tag-item>
     </ul>
   </aside>
 </template>
 
 <script>
-import AtomicNavItem from "./atomicNavItem";
-import AtomicTagItem from "./atomicTagItem";
-import atomicStructure from "../../build/atomicStructure.json";
+import AtomicNavItem from "./atomicNavItem"
+import AtomicTagItem from "./atomicTagItem"
+import atomicStructure from "../../build/atomicStructure.json"
 
 export default {
   name: "atomicNav",
@@ -94,46 +106,46 @@ export default {
 
   computed: {
     patterns() {
-      return this.atoms.concat(this.molecules, this.organisms);
+      return this.atoms.concat(this.molecules, this.organisms)
     },
     atoms() {
-      return this.flatten(this.atomicStructure.atoms.children);
+      return this.flatten(this.atomicStructure.atoms.children)
     },
     molecules() {
-      return this.flatten(this.atomicStructure.molecules.children);
+      return this.flatten(this.atomicStructure.molecules.children)
     },
     organisms() {
-      return this.flatten(this.atomicStructure.organisms.children);
+      return this.flatten(this.atomicStructure.organisms.children)
     }
   },
 
   methods: {
     flatten(items) {
-      let list = [];
+      let list = []
       for (let item of items) {
         if (item.isDataFile) {
-          list.push(item);
+          list.push(item)
         }
         if (item.children && item.children.length > 0) {
-          list = list.concat(this.flatten(item.children));
+          list = list.concat(this.flatten(item.children))
         }
       }
-      return list;
+      return list
     },
 
     addTag() {
       if (this.search.length) {
-        this.tags.push(this.search);
-        this.search = "";
+        this.tags.push(this.search)
+        this.search = ""
       }
     },
 
     removeTag(tag) {
-      let index = this.tags.indexOf(tag);
+      let index = this.tags.indexOf(tag)
       if (index >= 0) {
-        this.tags.splice(index, 1);
+        this.tags.splice(index, 1)
       }
     }
   }
-};
+}
 </script>
