63c7d10e3dc5bcfd5ee4d7fa76c27baa3e83e336
Simon Erlandsen <simon.erlandsen@gmail.com>
2019-09-18 12:51:58 +0200

    inserting html comments into blocks in twig compiler.

--- src/utils/twigCompiler.php
+++ src/utils/twigCompiler.php
@@ -35,6 +35,85 @@ function file_write_contents($dir, $contents){
 }
 
 /**
+ * insert_block_comments helper function: Get an array with the positions of every instance of a substring within a string
+ * @param $originalString
+ * @param $subString
+ */
+function get_substring_positions($originalString, $subString) {
+  $lastPos = 0;
+  $positions = array();
+
+  while (($lastPos = strpos($originalString, $subString, $lastPos)) !== false) {
+    $positions[] = $lastPos;
+    $lastPos += strlen($subString);
+  }
+
+  return $positions;
+}
+
+/**
+ * insert_block_comments helper function: Get an array of block positions within a twig string where the blocks between embed tags are filtered out
+ * @param $blockPositions
+ * @param $embedStartPositions
+ * @param $embedEndPositions
+ */
+function remove_embed_block_positions($blockPositions, $embedStartPositions, $embedEndPositions) {
+  // return out of the function with a terminal message if embed start and end positions are inconsistent
+  if (count($embedStartPositions) > count($embedEndPositions)) {
+    echo 'An embed is missing a closing tag.';
+    return false;
+  } else if (count($embedStartPositions) < count($embedEndPositions)) {
+    echo 'An embed is missing a start tag.';
+    return false;
+  }
+
+  $filteredPositions = $blockPositions;
+  // loop embed start/end tags
+  for($i = 0; $i < count($embedStartPositions); $i++) {
+    $filteredPositions = array_filter($filteredPositions, function($blockPosition) use ($embedStartPositions, $embedEndPositions, $i) {
+      // filtering out all block positions between embed start/end positions
+      return !($blockPosition > $embedStartPositions[$i] && $blockPosition < $embedEndPositions[$i]);
+    });
+  }
+
+  return $filteredPositions;
+}
+
+/**
+ * insert_block_comments helper function: Get a string where a substring is inserted at the position of another substring within an original string
+ * @param $originalString
+ * @param $stringToInsert
+ * @param $positions
+ */
+function insert_substring_at_positions($originalString, $stringToInsert, $positions) {
+  $stringWithSubStrings = $originalString;
+
+  $positionOffset = 0;
+  foreach ($positions as $value) {
+    $position = $value + $positionOffset;
+    $stringWithSubStrings = substr_replace($stringWithSubStrings, $stringToInsert, $position, 0);
+    $positionOffset = $positionOffset + strlen($stringToInsert);
+  }
+
+  return $stringWithSubStrings;
+}
+
+/**
+ * Get a twig string where a html comment is inserted into all blocks available to the consumer of the twig
+ * @param $twigContent
+ * @param $commentToInsert
+ */
+function insert_block_comments($twigContent, $commentToInsert) {
+  $blockPositions = get_substring_positions($twigContent, '{% endblock %}');
+  $embedStartPositions = get_substring_positions($twigContent, '{% embed');
+  $embedEndPositions = get_substring_positions($twigContent, '{% endembed %}');
+  $filteredPositions = remove_embed_block_positions($blockPositions, $embedStartPositions, $embedEndPositions);
+  $twigContentWithComments = insert_substring_at_positions($twigContent, "<!-- " . $commentToInsert . " -->\n", $filteredPositions);
+
+  return $twigContentWithComments;
+}
+
+/**
  * Render twig files
  */
 $twigData = json_decode(
@@ -74,8 +153,19 @@ if (is_writable(PROJECT_DIR . 'dist')) {
     }
 
     try {
+      $twigContent = file_get_contents(
+        getenv('AD_PATTERN_PATH') . $entry->src
+      );
+  
+      $twigWithComments = insert_block_comments(
+        $twigContent,
+        'your content here'
+      );
+
+      $template = $twig->createTemplate($twigWithComments);
+
       $compiled = $twig->render(
-        $entry->src,
+        $template,
         $mergedData
       );
 
