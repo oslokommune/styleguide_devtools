e3f439461a3fc86a337b9262cabbcafaead4cca7
Lars Boldt <boldt.lars@gmail.com>
2021-04-13 15:09:51 +0200

    button sizes

--- src/components/viewBox/viewBoxSettings.vue
+++ src/components/viewBox/viewBoxSettings.vue
@@ -4,30 +4,30 @@
       <div class="column">
         <h1 class="osg-heading-1" v-if="$store.state.pattern.settings.sections.title.visible">{{ title|capitalize }}</h1>
       </div>
-      <div class="column is-two-thirds osg-text-right chrome" v-if="$store.state.pattern.settings.sections.configuration.visible">
+      <div class="column is-two-thirds button-list osg-text-right chrome" v-if="$store.state.pattern.settings.sections.configuration.visible">
         <chrome v-model="color" />
-        <button class="osg-button osg-button--blue-light">
+        <button class="osg-button osg-button--blue-light osg-button--small">
           <i class="fas fa-palette"></i>
         </button>
-        <button class="osg-button" :class="{'osg-button--active': backgroundSolid}" @click="toggleSolidState">
+        <button class="osg-button osg-button--small" :class="{'osg-button--active': backgroundSolid}" @click="toggleSolidState">
           <i class="fas fa-fill"></i>
         </button>
-        <button :class="'osg-margin-left-3 osg-button' + (fullscreen ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="toggleFullscreen">
+        <button :class="'osg-margin-left-3 osg-button osg-button--small' + (fullscreen ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="toggleFullscreen">
           <i class="fas fa-expand" title="Fullscreen mode"></i>
         </button>
-        <button :class="'osg-margin-left-3 osg-button' + ($store.state.pattern.settings.viewSize.mobile ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('mobile')">
+        <button :class="'osg-margin-left-3 osg-button osg-button--small osg-button--blue-light' + ($store.state.pattern.settings.viewSize.mobile ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('mobile')">
           <i class="fas fa-mobile-alt" title="Mobile width"></i>
         </button>
-        <button :class="'osg-button' + ($store.state.pattern.settings.viewSize.tablet ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('tablet')">
+        <button :class="'osg-button osg-button--small osg-button--green-light' + ($store.state.pattern.settings.viewSize.tablet ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('tablet')">
           <i class="fas fa-tablet-alt" title="Tablet width"></i>
         </button>
-        <button :class="'osg-button' + ($store.state.pattern.settings.viewSize.desktop ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('desktop')">
+        <button :class="'osg-button osg-button--small osg-button--yellow' + ($store.state.pattern.settings.viewSize.desktop ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('desktop')">
           <i class="fas fa-desktop" title="Desktop width"></i>
         </button>
-        <button :class="'osg-button' + ($store.state.pattern.settings.viewSize.full ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('full')">
+        <button :class="'osg-button osg-button--small' + ($store.state.pattern.settings.viewSize.full ? ' osg-color-bg-blue-dark osg-color-text-white' : '')" @click="setViewSize('full')">
           <i class="fas fa-percentage" title="Full width"></i>
         </button>
-        <button class="osg-margin-left-3 osg-button" @click="resetToFactoryDefaults">
+        <button class="osg-margin-left-3 osg-button osg-button--small osg-button--red" @click="resetToFactoryDefaults">
           <i class="fas fa-industry"></i>
         </button>
       </div>
@@ -104,3 +104,10 @@
     }
   }
 </script>
+<style lang="scss" scoped>
+.button-list {
+  > button {    
+    margin-top: 23px
+  }
+}
+</style>
