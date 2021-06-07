b5445b225a94fa019203a71361bbd2855f51081c
Lars Boldt <boldt.lars@gmail.com>
2021-06-07 18:08:03 +0200

    switch eventlistener onchange/concurrently with chokidar

--- config/webpack.dev.cjs
+++ config/webpack.dev.cjs
@@ -1,8 +1,35 @@
-const webpack = require('webpack')
 const { merge } = require('webpack-merge')
+const { exec } = require('child_process');
+const chokidar = require('chokidar');
 const common = require('./webpack.common.cjs')
-const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+const styleguide = require('../src/styleguide.json')
+const webpack = require('webpack')
+
+chokidar.watch(styleguide.internal.project_path + '**/*', {
+  followSymlinks: false,
+  ignoreInitial: true
+}).on('all', (event, path) => {
+  let infoTerminalText = '\x1b[46m'
+  let redTerminalText = '\x1b[31m'
+  let resetTerminalColor = '\x1b[0m'
+  let styleguidePath = styleguide.internal.project_path.replace('./', '')
+
+  console.log("\n\n\n" + infoTerminalText + event.toUpperCase() + ": " + path.replace(styleguidePath, '') + resetTerminalColor + "\n")
+
+  let extension = path.substring(path.lastIndexOf('.') + 1)
+  if (event === 'addDir' || event === 'unlinkDir' || styleguide.internal.projectStructureFormats.indexOf(extension) !== -1) {
+    let cmd = 'yarn run build-structure'
+    if (styleguide.internal.buildJsFormats.indexOf(extension) !== -1) {
+      cmd = 'yarn run build'
+    }
+    exec(cmd, (error, stdout, stderr) => {
+      let formattedOutput = stdout.replace('ERROR', `${redTerminalText}ERROR`) + resetTerminalColor
+      console.log(formattedOutput)
+    })
+  }
+});
 
 module.exports = merge(common, {
   mode: 'development',
@@ -15,7 +42,14 @@ module.exports = merge(common, {
       chunkFilename: '[id].css'
     })
   ],
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].[chunkhash].js'
+  },
   cache: true,
+  optimization: {
+    runtimeChunk: true
+  },
   module: {
     rules: [
       {
@@ -41,7 +75,6 @@ module.exports = merge(common, {
 
 module.exports.devServer = {
   host: '0.0.0.0',
-  hot: true,
   port: 9000,
   static: [
     {
